0aa58fa3e2ce7fa9f2f27f9309ac8c718e686df9
polyqubit <polyumbrial@gmail.com>
2024-06-03 02:19:19 -0700

    fail

--- app/titration/data.tsx
+++ app/titration/data.tsx
@@ -3,15 +3,18 @@ export const list: {text: string}[] = [
         "text": "Click a button"
     },
     {
-        "text": "Indication"
+        "text": "Indicators($In$) are weak acids($HIn\\rightleftharpoons$$H^+ + In^-$). The indicator and its conjugate are of different colors $\\rarr$ by Le Chatelier's principle, the solution changes color with pH."
     },
     {
-        "text": "SAcid-SBase"
+        "text": "Plotted as pH vs. volume(of added titrant). The slow increase(or decrease if analyte is a base) of pH at beginning and sudden change around equivalence is due to logarithmic nature of the graph."
     },
     {
-        "text": "WAcid-Sbase"
+        "text": "sasaaa"
+    },
+    {
+        "text": "wasaaa"
     },
     {
         "text": "Polyumbrial"
-    },
+    }
 ]
--- app/titration/page.tsx
+++ app/titration/page.tsx
@@ -2,12 +2,14 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import Latex from "react-latex-next";
 import { useState } from "react";
 import { list } from "./data";
+import "/node_modules/katex/dist/katex.min.css"
 
 export default function Home() {
     const [num, setNum] = useState(0);
-    function handleClick(n:number) {
+    function handleClick(n: number) {
         setNum(n);
         // console.log(num);
     }
@@ -19,25 +21,32 @@ export default function Home() {
                 <Link className="border-2 border-white p-1" href="/">Back</Link>
             </div>
             <p className="para pb-1">
-                Determining the pH of buffer solution
+                Info on titration
             </p>
-            <ul>
+            <ul className="float-left pr-4">
                 <li>
-                    <button onClick={()=>handleClick(1)} className="button">Indicators</button>
+                    <button onClick={() => handleClick(1)} className="button">Indicators</button>
                 </li>
                 <li>
-                    <button onClick={()=>handleClick(2)} className="button">Strong Acid-Strong Base</button>
+                    <button onClick={() => handleClick(2)} className="button">Titration Curves</button>
                 </li>
                 <li>
-                    <button onClick={()=>handleClick(3)} className="button">Weak Acid-Strong Base</button>
+                    <button onClick={() => handleClick(3)} className="button">Strong Acid-Strong Base</button>
                 </li>
                 <li>
-                    <button onClick={()=>handleClick(4)} className="button">Polyprotic Acids</button>
+                    <button onClick={() => handleClick(4)} className="button">Weak Acid-Strong Base</button>
+                </li>
+                <li>
+                    <button onClick={() => handleClick(5)} className="button">Polyprotic Acids</button>
                 </li>
             </ul>
-            <p>
-                {current.text}
-            </p>
+            <div className="float-left border-2 p-1 border-stone-500">
+                <Latex>
+                    {current.text}
+                </Latex>
+                {num===2&&<p>testmachine</p>}
+            </div>
+
         </main>
     );
 }
